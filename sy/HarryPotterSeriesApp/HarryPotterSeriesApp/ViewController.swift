b60fae2498b6fb00ab8a91d5edd4b519df54a6d2
blueberry35 <ksyq12@daum.net>
2025-03-31 15:24:33 +0900

    Feat: Load books data in viewDidLoad

--- sy/HarryPotterSeriesApp/HarryPotterSeriesApp/ViewController.swift
+++ sy/HarryPotterSeriesApp/HarryPotterSeriesApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let dataService = DataService()
+    var books = Array<Book>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadBooks()
+        print(books)
     }
-
-
+    
+    func loadBooks() {
+        dataService.loadBooks { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let books):
+                self.books = books
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
